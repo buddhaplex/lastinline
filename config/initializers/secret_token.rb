b0494b5f68b81e81d69d21c622e71b4f1688bd0e
Eric Andrade <ericandrade@gmail.com>
2015-08-02 14:22:28 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lastinline::Application.config.secret_key_base = '38dcab167a632c2871f4d49342e0fc6b4dd270b7c1ced09be3812898f789fd84468e4bf8bc303f2b301b52bfca6f33a89db00fed64d6354684d950da65efa96a'
